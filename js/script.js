571f756bd55c254c30df1699876e61bd84c206d0
Lucas Becker <lucasbeckerfelisberto@gmail.com>
2020-10-30 19:33:34 -0300

    Update

--- js/script.js
+++ js/script.js
@@ -1,29 +1,62 @@
-/* --- Mostrar Menu --- */
+const containerMain = document.querySelector('main');
 
-function showMenu( toggleId, navId ){
-  const toggle = document.getElementById(toggleId);
-  const nav = document.getElementById(navId);
+var pathname = window.location.pathname; // Retornar só o path
+var url      = window.location.href;     // Retorna a url completa
+var anchorId = url.split("#")[url.split("#").length -1];
 
-  if( toggle && nav ){
-    toggle.addEventListener('click', () => {
-      nav.classList.toggle('show');
-    });
-  }
+console.log(pathname);
+console.log(url)
+
+
+function home(){
+  containerMain.innerHTML = `
+    <div class="name">
+      <h1>Lucas Becker</h1>
+    </div>
+    <div class="description">
+      <p>
+        Desenvolvedor Web Fullstack<br>
+        Freelancer
+      </p>
+    </div>
+    <div class="nav">
+      <a href="#curriculo" onclick="curriculum()">Currículo</a>
+      |
+      <a href="#portfolio" onclick="portfolio()">Portfolio</a>
+    </div>
+    <div class="links">
+      <a href="https://www.linkedin.com/in/lucasbeckerfelisberto" target="_blank">
+        <i class='bx bxl-linkedin'></i>
+      </a>
+      <a href="https://github.com/lucasbecker" target="_blank">
+        <i class='bx bxl-github' ></i>
+      </a>
+      <a href="https://codepen.io/lucasbecker" target="_blank">
+        <i class='bx bxl-codepen' ></i>
+      </a>
+      <a href="https://twitter.com/lucasbckr" target="_blank">
+        <i class='bx bxl-twitter' ></i>
+      </a>
+      <a href="https://www.instagram.com/lucas.bckr" target="_blank">
+        <i class='bx bxl-instagram' ></i>
+      </a>
+    </div>
+  `;
 }
 
-showMenu('nav-toggle', 'nav-menu');
-
-/* --- Ativar item do menu --- */
-const navLink = document.querySelectorAll('.nav__link');
-
-function linkAction(){
-  // Ativa link
-  navLink.forEach( n => n.classList.remove('active') );
-  this.classList.add('active');
-
-  // Remove menu mobile
-  const navMenu = document.getElementById('nav-menu');
-  navMenu.classList.remove('show');
+function curriculum(){
+  containerMain.innerHTML = `<a href="#" onclick="home()">Voltar</a><h1>Olá, visitante!</h1> <p>Ainda estamos trabalhando nesta página, desculpe o transtorno.</p>`;
 }
-
-navLink.forEach( n => n.addEventListener('click', linkAction) );
+function portfolio(){
+  containerMain.innerHTML = `<a href="#" onclick="home()">Voltar</a><h1>Olá, visitante!</h1> <p>Ainda estamos trabalhando nesta página, desculpe o transtorno.</p>`;
+}
+switch(anchorId){
+  case 'curriculo':
+    curriculum();
+    break;
+  case 'portfolio':
+    portfolio();
+    break;
+  default:
+    home();
+}
