6e0bef015424d5959679545c633cd7986ecea519
Lucas Becker <lucasbeckerfelisberto@gmail.com>
2024-01-16 00:15:19 -0300

    test: deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button>Hello World</Button>
+      <Button>Work In Progress</Button>
     </main>
   )
 }
